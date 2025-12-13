0d0c85766c1a2e2acbf002fcee4304744bb03d45
bharathkumar39293 <Bharath.ccbp@gmail.com>
2025-12-13 07:43:30 +0530

    Fix API endpoints and Vercel build configuration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
     const token = localStorage.getItem('token')
     try {
       if(token){
-        const { data } = await api.get('/api/users/data', {headers: {Authorization: token}})
+        const { data } = await api.get('/users/data', {headers: {Authorization: token}})
         if(data.user){
           dispatch(login({token, user: data.user}))
         }
